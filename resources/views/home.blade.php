a2c067bd26683406dfa61c6e04304b1ee62787e9
yhann07 <157436273+JamesLosito@users.noreply.github.com>
2025-05-20 13:54:21 +0800

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -181,7 +181,7 @@
     @include('components.navbar')
     <!-- Hero Section -->
     <div class="hero-section">
-        <img src="{{ asset('images/perfume.jpg') }}" class="hero-img" alt="Metro Essence Banner">
+        <img src="{{ asset('images/bg.jpg') }}" class="hero-img" alt="Metro Essence Banner">
         <div class="hero-content">
             <h1>ELEGANCE IN EVERY SCENT</h1>
             <p>Discover the essence of luxury with our exclusive perfume collection.</p>
